62e6d0d702c0605b858135efdd611d8543e803c8
anass <anassessalmany@gmail.com>
2026-04-14 12:36:00 +0100

    Add IsAdmin middleware and update product routes for admin access

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json(['message' => 'Ciob API v1']);
 });
+// Product routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+// Admin routes for managing products
+Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
+
