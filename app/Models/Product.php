de9f7ef71d629bd0bea21e2c651e20168f674202
anass <anassessalmany@gmail.com>
2026-04-16 08:46:31 +0100

    Refactor Category and Product models: add return type hints for relationships to improve code clarity

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Product extends Model
@@ -21,12 +23,12 @@ class Product extends Model
         ];
     }
 
-    public function category()
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
-    public function recipes()
+    public function recipes(): HasMany
     {
         return $this->hasMany(Recipe::class);
     }
