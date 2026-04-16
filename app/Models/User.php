9028fb2c3b911f4de826f4ceeb28394008626b18
anass <anassessalmany@gmail.com>
2026-04-16 08:35:48 +0100

    Refactor User model: remove unnecessary docblock for casts method and adjust formatting for clarity

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,16 +18,16 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->is_admin === true;
     }
 }
